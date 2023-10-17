53ffeac4a6d1bf912d354e35f78d7749328b9b06
Kevin Clark <kclark@cboe.com>
2023-10-17 07:04:53 -0500

    Adjust spaceship config

--- spaceship.zsh
+++ spaceship.zsh
@@ -10,19 +10,22 @@ SPACESHIP_DIR_TRUNC_REPO=false
 spaceship add git
 
 SPACESHIP_PROMPT_ORDER=(
-  user           # Username section
   dir            # Current directory section
-  host           # Hostname section
   git            # Git section (git_branch + git_status)
   python         # Python section
   rust           # Rust section
   lua            # Lua section
   venv           # virtualenv section
-  exec_time      # Execution time
   async          # Async jobs indicator
   line_sep       # Line break
-  jobs           # Background jobs indicator
-  exit_code      # Exit code section
   sudo           # Sudo indicator
   char           # Prompt character
+)
+
+SPACESHIP_RPROMPT_ORDER=(
+  user           # Username section
+  host           # Hostname section
+  jobs           # Background jobs indicator
+  exec_time      # Execution time
+  exit_code      # Exit code section
 )
